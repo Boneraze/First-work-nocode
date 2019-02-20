5acfd4498247ece630095993939248d58baffd91
Boneraze <43672196+Boneraze@users.noreply.github.com>
2019-02-20 16:31:30 +0600

    Add files via upload

---
+++ web_app/main.js
@@ -0,0 +1,5 @@
+function menu() {
+    document.querySelector('.burger-line').classList.toggle('burger-act');
+    document.querySelector('.super-button').classList.toggle('rotate');
+    document.querySelector('.menu').classList.toggle('changeWidth');
+}
