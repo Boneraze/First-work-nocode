b9a5f8e90dbcb74252f5891cc8303f6673010ddb
Boneraze <43672196+Boneraze@users.noreply.github.com>
2019-02-20 20:21:27 +0600

    Delete main.js

--- зкщоусе/main.js
+++
@@ -1,5 +0,0 @@
-function menu() {
-    document.querySelector('.burger-line').classList.toggle('burger-act');
-    document.querySelector('.super-button').classList.toggle('rotate');
-    document.querySelector('.menu').classList.toggle('changeWidth');
-}
