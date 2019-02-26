9260f11415ce7177f588985358be0e7f0c9ca173
Boneraze <43672196+Boneraze@users.noreply.github.com>
2019-02-26 22:43:39 +0600

    Add files via upload

--- web_app/main.js
+++ web_app/main.js
@@ -3,3 +3,11 @@ function menu() {
     document.querySelector('.super-button').classList.toggle('rotate');
     document.querySelector('.menu').classList.toggle('changeWidth');
 }
+
+function cubeRotate() {
+    document.querySelector('.cube-container').classList.toggle('rotateX120');
+}
+
+function clearCubeRotate() {
+    document.querySelector('.cube-container').classList.remove('rotateX120');
+}
